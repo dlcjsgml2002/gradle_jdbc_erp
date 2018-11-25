20ca3bd45a062f51eceeed2080d4d08e6d2f295c
fpvks <rephanoise@naver.com>
2018-11-26 01:20:15 +0900

    jdbc 추가 & connection test

---
+++ src/test/java/gradle_jdbc_erp/ConnectionProviderTest.java
@@ -0,0 +1,42 @@
+package gradle_jdbc_erp;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import gradle_jdbc_erp.jdbc.ConnectionProvider;
+import gradle_jdbc_erp.jdbc.LogUtil;
+
+public class ConnectionProviderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("Start ConnectionProviderTest");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("end ConnectionProviderTest");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println();
+	}
+
+	@Test
+	public void test() {
+		try (Connection connection = ConnectionProvider.getConnection()) {
+			LogUtil.prnLog(connection.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
